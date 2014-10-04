914d3fa01c61fbf153f8b1216806b5a4ba6dc7b0
Olivier Van Goethem <ov109@ic.ac.uk>
2014-10-04 15:42:16 +0100

    Linting code

--- src/main/java/com/pi/LightsApp.java
+++ src/main/java/com/pi/LightsApp.java
@@ -10,50 +10,53 @@ import automation.api.AbstractApp;
 import automation.api.components.NsLookupService;
 
 public class LightsApp extends AbstractApp {
-		
-	@Override
-	public void onStartup() {
-		try {
-			String deviceIp = NsLookupService.findDeviceIp("house-lights");
-			connectToRemoteDevice("http://" + deviceIp + ":8080/lights-companion-1.0.0/SocketController?wsdl", new QName("http://relay.pi.com/", "SocketControllerService"));
-		} catch (IOException e) {}
-	}
-	
-	public void turnOn() throws NoSuchMethodException  {
-		if (isDeviceAvailable()) {
-			device.invokeMethod("powerOn");
-		}
-	}
-	
-	public void turnOff() throws NoSuchMethodException {
-		if (isDeviceAvailable()) {
-			device.invokeMethod("powerOff");
-		}
-	}
-	
-	@Override
-	public String getState() throws NoSuchMethodException {
-		String state = "Device unavailable";
-		if (isDeviceAvailable()) {
-			state = "Power is currently " + (String) device.invokeMethod("getState") + "!";
-		}
-		return state;
-	}
-	
-	@Override
-	public String homeTile() throws NoSuchMethodException {
-		String state = "Device unavailable";
-		if (isDeviceAvailable()) {
-			state = "Power is currently " + (String) device.invokeMethod("getState") + "!";
-		}
-		return state;
-	}
 
-	@Override
-	public HashMap<String, Object> getModels() {
-		return new HashMap<String,Object>();
-	}
+    @Override
+    public void onStartup() {
+        try {
+            String deviceIp = NsLookupService.findDeviceIp("house-lights");
+            connectToRemoteDevice(
+                "http://" + deviceIp + ":8080/lights-companion-1.0.0/SocketController?wsdl",
+                new QName("http://relay.pi.com/", "SocketControllerService")
+            );
+        } catch (IOException e) {}
+    }
 
-	@Override
-	public void uploadFile(String fileName, File fileData) {}
+    public void turnOn() throws NoSuchMethodException  {
+        if (isDeviceAvailable()) {
+            device.invokeMethod("powerOn");
+        }
+    }
+
+    public void turnOff() throws NoSuchMethodException {
+        if (isDeviceAvailable()) {
+            device.invokeMethod("powerOff");
+        }
+    }
+
+    @Override
+    public String getState() throws NoSuchMethodException {
+        String state = "Device unavailable";
+        if (isDeviceAvailable()) {
+            state = "Power is currently " + (String) device.invokeMethod("getState") + "!";
+        }
+        return state;
+    }
+
+    @Override
+    public String homeTile() throws NoSuchMethodException {
+        String state = "Device unavailable";
+        if (isDeviceAvailable()) {
+            state = "Power is currently " + (String) device.invokeMethod("getState") + "!";
+        }
+        return state;
+    }
+
+    @Override
+    public HashMap<String, Object> getModels() {
+        return new HashMap<String,Object>();
+    }
+
+    @Override
+    public void uploadFile(String fileName, File fileData) {}
 }
